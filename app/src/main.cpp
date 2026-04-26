670bdf01ce3b72566642c1b7617e62b5199c3687
Ilya Tanko <ti1998.1998@gmail.com>
2026-04-26 23:33:43 +0200

    minimal change to blinky

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -11,16 +11,21 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-int main(void)
-{
+int main() {
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    if (!gpio_is_ready_dt(&led)) {
+        return 0;
+    }
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+        return 0;
+    }
 
-    while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+    while (true) {
+        if (gpio_pin_toggle_dt(&led) < 0) {
+            return 0;
+        }
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
