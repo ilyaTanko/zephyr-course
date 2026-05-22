390aa18e640c46c1bef11f5ad6ef6f4363cf9319
Ilya Tanko <ti1998.1998@gmail.com>
2026-05-22 23:32:29 +0200

    our driver shell extension commands

--- app/drivers/our_driver/our_driver_shell.c
+++ app/drivers/our_driver/our_driver_shell.c
@@ -1,5 +1,11 @@
+#include <errno.h>
+#include <ctype.h>
+#include <stdlib.h>
+
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/shell/shell.h>
+
+#include "our_driver.h"
 
 static int cmd_greet_handler(struct shell const* shell, size_t argc, char** argv) {
     shell_info(shell, "Hello from our driver!");
@@ -55,11 +61,37 @@ static int cmd_info_handler(struct shell const* shell, size_t argc, char** argv)
     return 0;
 }
 
+static int cmd_increment_handler(struct shell const* shell, size_t argc, char** argv) {
+    struct device const* dev = shell_device_get_binding(argv[1]);
+    if (dev == NULL) {
+        shell_error(shell, "Device '%s' not found", argv[1]);
+        return -ENODEV;
+    }
+
+    char* endptr;
+    errno = 0;
+    char const* value_start = argv[2];
+    while (isspace((unsigned char)*value_start)) {
+        value_start++;
+    }
+
+    uint32_t value = strtoul(value_start, &endptr, 0);
+    if ((errno == ERANGE) || (*endptr != '\0') || (endptr == argv[2]) || (*value_start == '-')) {
+        shell_error(shell, "Invalid value: %s", argv[2]);
+        return -EINVAL;
+    }
+
+    uint32_t result = our_driver_increment(dev, value);
+    shell_info(shell, "Increment result: %u", result);
+    return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(our_driver_subcmds,
     SHELL_CMD(hello, NULL, "Greeting from our driver", cmd_greet_handler),
     SHELL_CMD_ARG(read, NULL, "Get channel data", cmd_read_handler, 2, 0),
     SHELL_CMD_ARG(fetch, NULL, "Fetch sensor sample", cmd_fetch_handler, 2, 0),
     SHELL_CMD_ARG(info, NULL, "Print device name and ready state", cmd_info_handler, 2, 0),
+    SHELL_CMD_ARG(increment, NULL, "Increment counter by value", cmd_increment_handler, 3, 0),
     SHELL_SUBCMD_SET_END,
 );
 
